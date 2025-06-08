13aa269c55967517ccb95fb81fa8006400ddfa3a
Tublanx <rlgus03453@naver.com>
2025-06-08 21:44:24 +0900

    상품 정보를 지닌 클래스 생성

---
+++ FlipMarketAndroid/app/src/main/java/com/lgh/flipmarketandroid/dto/product/Product.java
@@ -0,0 +1,64 @@
+package com.lgh.flipmarketandroid.dto.product;
+
+public class Product {
+
+    private String productName;
+
+    private int price;
+
+    private String category;
+
+    private String description;
+
+    private String imagePath;
+
+    private int stock;
+
+    private int likeCount;
+
+    public Product() {}
+
+    public Product(String productName, int price, String imagePath) {
+        this.productName = productName;
+        this.price = price;
+        this.imagePath = imagePath;
+    }
+
+    public Product(String productName, int price, String category, String description, String imagePath, int stock, int likeCount) {
+        this.productName = productName;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+        this.imagePath = imagePath;
+        this.stock = stock;
+        this.likeCount = likeCount;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+}
