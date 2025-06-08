d9a5ad19286836d18df76873940597e1ea01e928
Tublanx <rlgus03453@naver.com>
2025-06-08 21:44:05 +0900

    이미지를 로드하는 의존성 추가

--- FlipMarketAndroid/app/build.gradle.kts
+++ FlipMarketAndroid/app/build.gradle.kts
@@ -63,6 +63,9 @@ dependencies {
     // CardView 의존성
     implementation("androidx.cardview:cardview:1.0.0")
 
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
     androidTestImplementation(platform(libs.compose.bom))
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
