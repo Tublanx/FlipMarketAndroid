80ac241f4202cd903f2a670e68e7fcb9e4095ccf
Tublanx <rlgus03453@naver.com>
2025-06-11 15:16:51 +0900

    마이페이지, 장바구니 페이지 템플릿 추가

---
+++ FlipMarketAndroid/app/src/main/java/com/lgh/flipmarketandroid/activity/CartActivity.java
@@ -0,0 +1,17 @@
+package com.lgh.flipmarketandroid.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lgh.flipmarketandroid.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
